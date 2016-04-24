cc820a5a565235157b9621da407bddd7d7a38af6
paulo <paulorec@gmail.com>
2016-04-24 12:42:34 -0300

    Adding scripts for player and camerda

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	//Physics code
+	void FixedUpdate() {
+
+		float moveHori = Input.GetAxis ("Horizontal");
+		float moveV = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHori, 0.0f, moveV);
+
+		rb.AddForce (movement * speed);
+	}
+}
